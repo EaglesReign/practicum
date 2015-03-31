0513aa8a4cfe8bd3fed6c8f1e99c05151cf18c4b
Phillip <phv9495@saintjoe.edu>
2015-03-30 20:01:58 -0400

    Add code for counter.js

--- counter.js
+++ counter.js
@@ -1,10 +1,14 @@
-var stock = "1 lemon, 2 cabbages, and 101 eggs";
+//changed eggs to have 102.
+var stock = "1 lemon, 2 cabbages, and 102 eggs";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
     amount = "no";
+  //added this portion so that if amount exceeds 100, you have zillions.
+  else if (amount > 100)
+    amount = "zillions of";
   return amount + " " + unit;
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
